e68db42f6cd3f53b2cefffb43a678832e03ede8f
Jon Waide <jonwaide@yahoo.com>
2025-01-06 08:05:15 -0800

    Reverting to source from classs example

--- main.tf
+++ main.tf
@@ -48,14 +48,15 @@ resource "aws_instance" "blog" {
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -63,23 +64,17 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets          = {
-        my_target = {
-           target_id = aws_instance.blog.id
-           port      = 80
-        }
-      }
     }
   ] 
 
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
-  }
-
+  ]
+  
   tags = {
     Environment = "dev"
   }
